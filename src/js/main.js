648bb3b60c25e74af69926192dcae3459dbd3597
Hugo Dutra <hugow.dutra@gmail.com>
2016-08-16 02:16:57 -0300

    pupulating the map

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,23 @@
+
 global.$       = global.jQuery = require('jquery');
 
-var axios = require('axios');
-var Papa = require('../../node_modules/papaparse/papaparse.js');
+const axios = require('axios');
+const Papa = require('../../node_modules/papaparse/papaparse.js');
+const L = require('leaflet');
+
 
 let parsedCsv;
+let map = L.map('map');
+let attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'; 
+let tiles = 'https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaHVnb2R1dHJhIiwiYSI6ImNpcnd5eHA2eDAwM2oybm53emxtdzRpYXcifQ.rQib4fLiSiySuIuh-zLmkw';
+ 
+L.Icon.Default.imagePath = '../images/leaflet';
+L.tileLayer(tiles, {
+  maxZoom: 18,
+  attribution: attribution
+}).addTo(map);
+
+map.setView([37.472189, -122.190191], 11);
 
 axios.get('sample.csv')
 	.then((response) => {	
@@ -18,5 +32,6 @@ axios.get('sample.csv')
 			for(let j=0, ln=parsedCsv[i].length; j<ln; j++) {
 				$(`.js-table tbody tr[data-key=${i}]`).append(`<td>${parsedCsv[i][j]}</td>`)
 			}
+			L.marker([parsedCsv[i][9], parsedCsv[i][10]]).addTo(map)
 		}
 	})
